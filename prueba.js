adad054daaad46ddfb7c219b4dbde4aaabb698cc
Marcos <marcos@MacBook-Pro-de-Marcos.local>
2019-01-13 21:27:41 -0300

    Ejercicios con condicionales

---
+++ prueba.js
@@ -0,0 +1,48 @@
+// Primer ejercicio
+// var numero;
+// numero = prompt('Ingresa un numero: ', '');
+
+// if ( numero == 10 ) {
+//     document.write('El número es igual a 10');
+// } else {
+//     document.write('El número no es igual a 10');
+// }
+
+// Segundo ejercicio
+// var numero1;
+// var numero2;
+// numero1 = prompt('Ingresa el primer número: ');
+// numero2 = prompt('Ingresa el segundo número: ');
+
+// if ( numero1 == numero2 ) {
+//     document.write('Son iguales');
+// } else {
+//     document.write('No son iguales');
+// }
+
+// Tercero ejercicio
+// var numero1;
+// var numero2;
+// numero1 = prompt('Ingresa el primer número: ');
+// numero2 = prompt('Ingresa el segundo número: ');
+// numero1 = parseInt(numero1);
+// numero2 = parseInt(numero2);
+// if ( numero1 == numero2 ) {
+//     document.write('Son iguales');
+// } else if(numero1 > numero2){
+//     document.write('numero1 es mayor');
+// } else {
+//     document.write('numero2 es mayor');
+// }
+
+// Cuarto ejercicio
+// var password1;
+// var password2;
+// password1 = prompt('Ingrese su clave');
+// password2 = prompt('Volve a ingresar tu clave');
+
+// if ( password1 === password2 ) {
+//     document.write('Iniciar app...');
+// } else {
+//     document.write('Verificar clave');
+// }
